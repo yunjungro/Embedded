69d5971c86bd2c60b9c4c9131b6425815a276743
yunjungro <ganz1378@naver.com>
2025-06-19 21:03:58 +0900

    Initial commit

---
+++ led.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <ctype.h>
+#include <sys/ipc.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include "ledtest.h"
+
+static unsigned int ledValue = 0;
+static int fd = 0;
+
+int ledLibInit() {
+    fd = open(LED_DRIVER_NAME, O_WRONLY);
+    ledValue = 0;
+    return fd;
+}
+int ledOnOff(int ledNum, int onOff) {
+    int i = 1;
+    i = i << ledNum;
+    ledValue = ledValue & (~i);
+    if (onOff != 0) {
+        ledValue |= i;
+    }
+
+    write(fd, &ledValue, 4);
+}
+int ledStatus() {
+    return ledValue;
+}
+int ledLibExit() {
+    ledValue = 0;
+    ledOnOff(0, 0);
+    close(fd);
+    return 0;
+}
